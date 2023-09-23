e03aa8ff1f325697c1987e78ba603ed80fc7ba1a
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-09-23 14:31:40 +0100

    Added a custom welcome page

--- wp-content/plugins/simply-static/vendor/voku/simple_html_dom/build/generate_docs.php
+++ wp-content/plugins/simply-static/vendor/voku/simple_html_dom/build/generate_docs.php
@@ -1,16 +1,7 @@
-<?php
-
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/vendor/autoload.php';
-
-$readmeText = (new \voku\PhpReadmeHelper\GenerateApi())->generate(
-    __DIR__ . '/../src/',
-    __DIR__ . '/docs/api.md',
-    [
-        \voku\helper\DomParserInterface::class,
-        \voku\helper\SimpleHtmlDomNodeInterface::class,
-        \voku\helper\SimpleHtmlDomInterface::class
-    ]
-);
-
-file_put_contents(__DIR__ . '/../README_API.md', $readmeText);
+<br>
+<b>Warning</b>:  require(D:\xampp\htdocs\georgi-wordpress-website\wp-content\plugins\simply-static\vendor\voku\simple_html_dom\build/../vendor/autoload.php): Failed to open stream: No such file or directory in <b>D:\xampp\htdocs\georgi-wordpress-website\wp-content\plugins\simply-static\vendor\voku\simple_html_dom\build\generate_docs.php</b> on line <b>3</b><br>
+<br>
+<b>Fatal error</b>:  Uncaught Error: Failed opening required 'D:\xampp\htdocs\georgi-wordpress-website\wp-content\plugins\simply-static\vendor\voku\simple_html_dom\build/../vendor/autoload.php' (include_path='D:\xampp\php\PEAR') in D:\xampp\htdocs\georgi-wordpress-website\wp-content\plugins\simply-static\vendor\voku\simple_html_dom\build\generate_docs.php:3
+Stack trace:
+#0 {main}
+  thrown in <b>D:\xampp\htdocs\georgi-wordpress-website\wp-content\plugins\simply-static\vendor\voku\simple_html_dom\build\generate_docs.php</b> on line <b>3</b><br>
