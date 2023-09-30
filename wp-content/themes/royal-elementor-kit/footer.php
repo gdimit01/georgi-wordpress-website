f8fda195d141ee38350fb2d22e3114292826a7c6
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-09-30 20:11:25 +0100

    minor fix

--- wp-content/themes/royal-elementor-kit/footer.php
+++ wp-content/themes/royal-elementor-kit/footer.php
@@ -1,23 +1,24 @@
 		<!-- Page Footer -->
 		<footer id="page-footer">
 
-			<div class="footer-copyright">
-				
-				<div class="credit">
-					<?php
+		    <div class="footer-copyright">
+
+		        <div class="credit">
+		            <?php
 					$theme_data	= wp_get_theme();
 					/* translators: %1$s: theme name, %2$s link, %3$s theme author */
 					printf( __( '%1$s Theme by <a href="%2$s">%3$s.</a>', 'royal-elementor-kit' ), esc_html( $theme_data->Name ), esc_url( 'https://royal-elementor-addons.com/' ), $theme_data->Author );
 					?>
-				</div>
+		        </div>
 
-			</div>
-			
+		    </div>
+
 		</footer><!-- #page-footer -->
 
-	</div><!-- #page-wrap -->
+		</div><!-- #page-wrap -->
 
-<?php wp_footer(); ?>
+		<?php wp_footer(); ?>
 
-</body>
-</html>
+		</body>
+
+		</html>
--- wp-content/themes/royal-elementor-kit/header.php
+++ wp-content/themes/royal-elementor-kit/header.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-<?php wp_head(); ?>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-	<?php
+    <?php
 	if ( function_exists( 'wp_body_open' ) ) {
 	    wp_body_open();
 	} else {
@@ -18,15 +19,16 @@
 	?>
 
 
-	<!-- Page Wrapper -->
-	<div id="page-wrap">
+    <!-- Page Wrapper -->
+    <div id="page-wrap">
 
-	<a class="skip-link screen-reader-text" href="#skip-link-target"><?php _e( 'Skip to content', 'royal-elementor-kit' ); ?></a>
+        <a class="skip-link screen-reader-text"
+            href="#skip-link-target"><?php _e( 'Skip to content', 'royal-elementor-kit' ); ?></a>
 
-	<header id="site-header" class="site-header" role="banner">
+        <header id="site-header" class="site-header" role="banner">
 
-		<div class="site-logo">
-			<?php
+            <div class="site-logo">
+                <?php
 
 			if ( has_custom_logo() ) :
 				the_custom_logo();
@@ -35,25 +37,26 @@
 
 			?>
 
-				<h1 class="site-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( 'Home', 'royal-elementor-kit' ); ?>" rel="home">
-						<?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-					</a>
-				</h1>
+                <h1 class="site-title">
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+                        title="<?php esc_attr_e( 'Home', 'royal-elementor-kit' ); ?>" rel="home">
+                        <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+                    </a>
+                </h1>
 
-				<?php if ( get_bloginfo( 'description', 'display' ) ) : ?>
-				<p class="site-description">
-					<?php echo esc_html( get_bloginfo( 'description', 'display' ) ); ?>
-				</p>
-				<?php endif; ?>
+                <?php if ( get_bloginfo( 'description', 'display' ) ) : ?>
+                <p class="site-description">
+                    <?php echo esc_html( get_bloginfo( 'description', 'display' ) ); ?>
+                </p>
+                <?php endif; ?>
 
-			<?php endif; ?>
-		</div>
+                <?php endif; ?>
+            </div>
 
-		<?php if ( has_nav_menu('main') ) : ?>
-		<nav class="main-navigation" role="navigation">
-			<?php wp_nav_menu( [ 'theme_location' => 'main' ] ); ?>
-		</nav>
-		<?php endif; ?>
+            <?php if ( has_nav_menu('main') ) : ?>
+            <nav class="main-navigation" role="navigation">
+                <?php wp_nav_menu( [ 'theme_location' => 'main' ] ); ?>
+            </nav>
+            <?php endif; ?>
 
-	</header>
+        </header>
