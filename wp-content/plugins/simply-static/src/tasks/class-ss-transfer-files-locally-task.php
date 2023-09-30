7429435ac7fa35b978ea3fdbf3d4b73d523a3762
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-09-30 21:25:18 +0100

    m

---
+++ wp-content/plugins/simply-static/src/tasks/class-ss-transfer-files-locally-task.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Class &quot;Simply_Static\Task&quot; not found in D:\xampp\htdocs\georgi-wordpress-website\wp-content\plugins\simply-static\src\tasks\class-ss-transfer-files-locally-task.php:8
+Stack trace:
+#0 {main}
+  thrown in <b>D:\xampp\htdocs\georgi-wordpress-website\wp-content\plugins\simply-static\src\tasks\class-ss-transfer-files-locally-task.php</b> on line <b>8</b><br>
