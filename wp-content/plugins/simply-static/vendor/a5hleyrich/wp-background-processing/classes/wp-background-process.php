abb584615147b24dd8ca0cb0979f5ca4ab5532a8
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-09-30 21:21:11 +0100

    m

--- wp-content/plugins/simply-static/vendor/a5hleyrich/wp-background-processing/classes/wp-background-process.php
+++
@@ -1,5 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Class &quot;WP_Async_Request&quot; not found in D:\xampp\htdocs\georgi-wordpress-website\wp-content\plugins\simply-static\vendor\a5hleyrich\wp-background-processing\classes\wp-background-process.php:14
-Stack trace:
-#0 {main}
-  thrown in <b>D:\xampp\htdocs\georgi-wordpress-website\wp-content\plugins\simply-static\vendor\a5hleyrich\wp-background-processing\classes\wp-background-process.php</b> on line <b>14</b><br>
--- wp-content/plugins/simply-static/vendor/symfony/css-selector/Parser/Parser.php
+++
@@ -1,5 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Interface &quot;Symfony\Component\CssSelector\Parser\ParserInterface&quot; not found in D:\xampp\htdocs\georgi-wordpress-website\wp-content\plugins\simply-static\vendor\symfony\css-selector\Parser\Parser.php:28
-Stack trace:
-#0 {main}
-  thrown in <b>D:\xampp\htdocs\georgi-wordpress-website\wp-content\plugins\simply-static\vendor\symfony\css-selector\Parser\Parser.php</b> on line <b>28</b><br>
