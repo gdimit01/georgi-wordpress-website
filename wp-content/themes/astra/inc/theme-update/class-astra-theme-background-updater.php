ddd7d5aedf3760e72645258f5fe0b8e97a84da9e
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-09-30 20:36:39 +0100

    m

--- wp-content/themes/astra/inc/theme-update/class-astra-theme-background-updater.php
+++
@@ -1,6 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Call to undefined function is_admin() in D:\xampp\htdocs\georgi-wordpress-website\wp-content\themes\astra\inc\theme-update\class-astra-theme-background-updater.php:140
-Stack trace:
-#0 D:\xampp\htdocs\georgi-wordpress-website\wp-content\themes\astra\inc\theme-update\class-astra-theme-background-updater.php(430): Astra_Theme_Background_Updater-&gt;__construct()
-#1 {main}
-  thrown in <b>D:\xampp\htdocs\georgi-wordpress-website\wp-content\themes\astra\inc\theme-update\class-astra-theme-background-updater.php</b> on line <b>140</b><br>
--- wp-content/themes/royal-elementor-kit/header.php
+++ wp-content/themes/royal-elementor-kit/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<html></html><html>
+<html><html>
 <b>Fatal error</b>:  Uncaught Error: Call to undefined function language_attributes() in D:\xampp\htdocs\georgi-wordpress-website\wp-content\themes\royal-elementor-kit\header.php:2
 Stack trace:
 #0 {main}
   thrown in <b>D:\xampp\htdocs\georgi-wordpress-website\wp-content\themes\royal-elementor-kit\header.php</b> on line <b>2</b><br>
-</html>
+</html></html>
